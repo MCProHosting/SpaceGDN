51652ef136ab4337b277f55e2b5c76f2f4617a86
Innectic <innectic@gmail.com>
2018-07-18 11:07:47 -0700

    Fix mojang loader versions

--- loader/interfaces/mojang_loader.py
+++ loader/interfaces/mojang_loader.py
@@ -5,16 +5,16 @@ class loader_mojang:
 
     url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
 
-    def getJSON(self):
+    def get_json(self):
         response = urllib2.urlopen(self.url)
         return json.loads(response.read())
 
-    def totimestamp(self, dt, epoch=datetime.datetime(1970,1,1)):
+    def to_timestamp(self, dt, epoch=datetime.datetime(1970,1,1)):
         td = dt - epoch
         return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 1e6 
 
     def load(self, channel, last_build):
-        data = self.getJSON()
+        data = self.get_json()
 
         builds = []
         for build in data['versions']:
@@ -26,7 +26,7 @@ class loader_mojang:
             # HACK: This is really really bad and needs to be fixed ASAP
             if minecraft_version > StrictVersion("1.7.8"):
                 time = datetime.datetime.strptime(re.sub(r'\+[0-9]{2}:[0-9]{2}$', '', build['releaseTime']), '%Y-%m-%dT%H:%M:%S')
-                build_number = int(self.totimestamp(time))
+                build_number = int(self.to_timestamp(time))
 
                 builds.append({
                     'version': build['id'],
